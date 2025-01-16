b9164ea18cf848c3731ddec852612cfed13dab42
unknown <jessicagualtieri@hotmail.fr>
2025-01-16 17:07:09 +0100

    Working on day three !

---
+++ DayThree/DayThree/Program.cs
@@ -0,0 +1,14 @@
+﻿string fileName = "input.txt";
+string currentDirectory = AppContext.BaseDirectory;
+string filePath = Path.Combine(currentDirectory, fileName);
+StreamReader streamReader = new StreamReader(filePath);
+string? line;
+int total = 0;
+
+
+while (!streamReader.EndOfStream)
+{
+    line = streamReader.ReadLine();
+}
+
+Console.WriteLine(total);
