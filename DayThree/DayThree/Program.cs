f6dd58977b0975029c8c8d518ab0f7fb1a18b494
unknown <jessicagualtieri@hotmail.fr>
2025-01-16 17:21:02 +0100

    Regex seems to be working fine

--- DayThree/DayThree/Program.cs
+++ DayThree/DayThree/Program.cs
@@ -1,14 +1,22 @@
-﻿string fileName = "input.txt";
+﻿using System.Text.RegularExpressions;
+
+string fileName = "input.txt";
 string currentDirectory = AppContext.BaseDirectory;
 string filePath = Path.Combine(currentDirectory, fileName);
 StreamReader streamReader = new StreamReader(filePath);
 string? line;
 int total = 0;
+string pattern = @"mul\(\d{1,3},\d{1,3}\)";
 
 
 while (!streamReader.EndOfStream)
 {
     line = streamReader.ReadLine();
+    MatchCollection matchCollection = Regex.Matches(line, pattern);
+    foreach (Match match in matchCollection)
+    {
+        Console.WriteLine(match.Value);
+    }
 }
 
 Console.WriteLine(total);
--- DayThree/DayThree/obj/Debug/net8.0/DayThree.AssemblyInfo.cs
+++ DayThree/DayThree/obj/Debug/net8.0/DayThree.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DayThree")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d434d91ad8743b1e75ef7c28c73798ecbeb7fde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9164ea18cf848c3731ddec852612cfed13dab42")]
 [assembly: System.Reflection.AssemblyProductAttribute("DayThree")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DayThree")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
